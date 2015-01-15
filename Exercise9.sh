48d16b346c8cd9d8dbd12d2fb60e224b617079fa
astridbos <astrid.bos@wur.nl>
2015-01-15 19:23:08 +0100

    layout

--- Exercise9.sh
+++ Exercise9.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-echo ""
-echo "AD-Scripting team"
-echo "Names: David Scholte-Albers & Astrid Bos"
-echo "Exercise 9, on "Getting started with Linux OS-GEOLive""
-echo ""
-echo "test"
+echo '
+      Exercise 9, on "Getting started with Linux OS-GEOLive"'
+echo -en '\n'
+echo -e '\033[1m                         AD-Scripting team\033[0m'
+echo -e '\033[1m                David Scholte-Albers & Astrid Bos\033[0m'
+echo -en '\n'
 
